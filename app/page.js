26484a50faa81d1d267da56ad87cba0c9fc211da
Sakib-05 <sakibimamul03@gmail.com>
2024-08-04 14:57:53 +0100

    creating the table with the items

--- app/page.js
+++ app/page.js
@@ -2,15 +2,86 @@
 import Image from "next/image";
 import {useState, useEffect} from 'react'
 import {firestore} from '@/firebase'
-import { Box, Typography } from "@mui/material";
+import { Box, Modal, Typography } from "@mui/material";
+import { collection, deleteDoc, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
-  const [open, setOpen] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const upadateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({ 
+        name: doc.id,
+        ...doc.data(), 
+      });
+    });
+    setInventory(inventoryList);
+  }
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'),item);
+    const docSnap = await getDocs(docRef);
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity+1});
+    }
+    else{
+      await setDoc(docRef, {quantity: 1});
+    }
+    await upadateInventory();
+  }
+
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'),item);
+    const docSnap = await getDocs(docRef);
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data();
+      if(quantity ===1){
+        await deleteDoc(docRef);
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity-1});
+      }
+    }
+    await upadateInventory();
+  }
+
+
+  useEffect(() => {
+    upadateInventory()
+  }, [])
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   
+  
+
   return (
-    <Box>
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      justifyContent="center" 
+      alignItems="center"
+      gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box>
+
+
+        </Box>
+      </Modal>
       <Typography variant="h1">Inventory management</Typography>
+      
+
+
     </Box>
 
   );
